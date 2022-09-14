df65cd7553f5b7c89bedd6e420643fa980aa6df2
kele <40495743+kele-sync@users.noreply.github.com>
2022-09-14 21:19:02 +0800

    init

---
+++ src/main/java/com/wrench/keleServer/KeleServerApplication.java
@@ -0,0 +1,13 @@
+package com.wrench.keleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wrench/keleServer/KeleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrench.keleServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
